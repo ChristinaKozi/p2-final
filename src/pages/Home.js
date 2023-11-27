a32ba600e5babcfaea38c3c905f2e6e6be477515
Christina Kozanian <christinakozanian@gmail.com>
2023-11-27 13:29:58 -0800

    adds home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    
+    return (
+        <div>
+            <h1>My Home</h1>
+            <hr />
+            <p>Decribe what app does</p>
+        </div>
+    )
+};
+
+export default Home;
