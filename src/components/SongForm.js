79a4be02045fd9b51c8530218810f2f34bf2ed2f
Christina Kozanian <christinakozanian@gmail.com>
2023-11-27 20:28:44 -0800

    SongForm component added

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -3,10 +3,7 @@ import React from "react";
 function SongForm(){
     return (
         <>
-            <form>
-                <label>Title:</label>
-                <input type='text'></input>
-            </form>
+
         </>
     )
 }
