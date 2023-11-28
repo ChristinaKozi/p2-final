edcde43bcde14aee42dae58b852a9f9d83da13ca
Christina Kozanian <christinakozanian@gmail.com>
2023-11-27 20:32:41 -0800

    updates form

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -3,7 +3,11 @@ import React from "react";
 function SongForm(){
     return (
         <>
-
+            <form>
+                <label>Title:</label>
+                <input type='text'></input>
+                
+            </form>
         </>
     )
 }
