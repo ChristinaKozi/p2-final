ab91f57741fe37ce68856f91a8434ff8c1e56910
Christina Kozanian <christinakozanian@gmail.com>
2023-11-28 02:38:36 -0800

    updates text in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,12 @@ function Home() {
             <header>
                 <NavBar />
             </header>
-            <h1>My Home</h1>
+            <h1>Home</h1>
             <hr />
-            <p>Decribe what app does</p>
+            <h2>Welcome to Genre Garage Band Practice!</h2>
+            <p>To begin, click on the Songs tab above and you will find the Genre Generator. 
+                The genrenator will provide you with a genre which you can then apply to your favorite songs in your song list and practice in that style. If you're feeling inspired, you can write a new song using the generated genre!
+            </p>
         </div>
     )
 };
