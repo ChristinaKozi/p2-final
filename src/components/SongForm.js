215709318b77dd104090bc772e3ad1d3bb003221
Christina Kozanian <christinakozanian@gmail.com>
2023-11-27 20:33:11 -0800

    updates SongForm

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -4,9 +4,18 @@ function SongForm(){
     return (
         <>
             <form>
-                <label>Title:</label>
+                <label>Title: </label>
+                <input type='text'></input> 
+                <br />
+                <br />
+
+                <label>Artist: </label>
                 <input type='text'></input>
-                
+                <br />
+                <br />
+
+                <label>Lyrics: </label>
+                <textarea type='text'></textarea> 
             </form>
         </>
     )
